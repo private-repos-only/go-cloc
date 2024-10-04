97d3929520e8cff84629cbbf9528699fface24b4
Cole Gannaway <156719330+cole-gannaway-sonarsource@users.noreply.github.com>
2024-10-04 16:11:35 -0500

    fixed build.sh for windows

--- build.sh
+++ build.sh
@@ -12,20 +12,24 @@ BINARY_NAME="go-cloc" # Change this to your program's name
 mkdir -p $OUTPUT_DIR
 
 # Define platforms and architectures
-declare -A platforms
 platforms=(
-    ["linux/amd64"]="linux_amd64"
-    ["linux/arm64"]="linux_arm64"
-    ["darwin/amd64"]="darwin_amd64"
-    ["darwin/arm64"]="darwin_arm64"
-    ["windows/amd64"]="windows_amd64"
-    ["windows/386"]="windows_386"
+    "linux/amd64"
+    "linux/arm64"
+    "darwin/amd64"
+    "darwin/arm64"
+    "windows/amd64"
+    "windows/386"
 )
 
 # Build for each platform and create a zip for each
-for platform in "${!platforms[@]}"; do
+for platform in "${platforms[@]}"; do
     IFS="/" read -r os arch <<< "$platform"
-    output_file="$OUTPUT_DIR/$BINARY_NAME"
+    
+    if [ "$os" == "windows" ]; then
+        output_file="$OUTPUT_DIR/$BINARY_NAME.exe"
+    else
+        output_file="$OUTPUT_DIR/$BINARY_NAME"
+    fi
     
     echo "Building for $os/$arch..."
     GOOS=$os GOARCH=$arch go build -o "$output_file" main.go
@@ -34,9 +38,9 @@ for platform in "${!platforms[@]}"; do
         echo "Build failed for $os/$arch"
         exit 1
     fi
-    
+  
     # Create a zip file for the current build
-    zip_file="$OUTPUT_DIR/${BINARY_NAME}-${platforms[$platform]}.zip"
+    zip_file="$OUTPUT_DIR/${BINARY_NAME}-${os}-${arch}.zip"
     zip -j "$zip_file" "$output_file"
     
     # Optionally remove the binary after zipping
